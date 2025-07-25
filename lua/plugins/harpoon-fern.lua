cbb8d0caf0e87809e4b5a2ed77fe4051a17ff182
dragonlobster <zai.keung@gmail.com>
2025-07-25 18:07:30 +0800

    added harpoon-fern to fern config

--- lua/plugins/harpoon-fern.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "dragonlobster/harpoon-fern.nvim",
-    dependencies = { "harpoon", "vim-fern", "plenary.nvim" },
-    config = function()
-        local configs = require("harpoon-fern")
-        configs.setup()
-    end
-}
