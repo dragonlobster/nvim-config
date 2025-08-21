10b79e6c3eba1ae99a7332318464fa814a55ee06
dragonlobster <andrew@aftergate.io>
2025-08-21 20:20:19 +0800

    updated codecompanion

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -50,29 +50,59 @@ return {
 
     {
         "olimorris/codecompanion.nvim",
-        opts = {
-            strategies = {
-                chat = {
-                    adapter = "copilot"
-                }
-            },
-
-            display = {
-                action_palette = {
-                    prompt = vim.fn.nr2char(0x1f916)
-                },
-                chat = {
-                    window = {
-                        width = 0.35,
-                        opts = {
-                            winfixwidth = true,
-                            number = false
+        opts =
+            function()
+                local local_adapter = "copilot"
+                if os.getenv("OPENROUTER_API_KEY")
+                then
+                    local_adapter = "openrouter"
+                end
+                return
+                {
+                    strategies = {
+                        chat = {
+                            adapter = local_adapter
+                        },
+                        inline = {
+                            adapter = local_adapter
                         }
-                    }
-                }
-            },
+                    },
 
-        },
+                    adapters = {
+                        openrouter = function()
+                            return require("codecompanion.adapters").extend("openai_compatible", {
+                                env = {
+                                    url = "https://openrouter.ai/api",
+                                    api_key = os.getenv("OPENROUTER_API_KEY"),
+                                    chat_url = "/v1/chat/completions",
+                                },
+                                schema = {
+                                    model = {
+                                        default = "anthropic/claude-3.7-sonnet",
+                                    },
+                                },
+                            })
+                        end,
+                    },
+
+                    display = {
+                        action_palette = {
+                            prompt = vim.fn.nr2char(0x1f916)
+                        },
+                        chat = {
+                            window = {
+                                width = 0.35,
+                                opts = {
+                                    winfixwidth = true,
+                                    number = false
+                                }
+                            }
+                        }
+                    },
+
+                }
+            end
+        ,
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-treesitter/nvim-treesitter",
