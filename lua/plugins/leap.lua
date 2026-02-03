222294b9af6bc89b0aa82e50de24f1e0783ceaf3
dragonlobster <zai.keung@gmail.com>
2026-02-03 12:15:49 +0800

    updated leap

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,10 +1,24 @@
 return {
-    "ggandor/leap.nvim",
+    url = "https://codeberg.org/andyg/leap.nvim",
     dependencies = { "tpope/vim-repeat" },
     config = function()
-        require("leap").create_default_mappings()
+        -- require("leap").create_default_mappings()
 
         -- TODO: add group
+        vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+        vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+
+        -- Highly recommended: define a preview filter to reduce visual noise
+        -- and the blinking effect after the first keypress
+        -- (see `:h leap.opts.preview`).
+        -- For example, skip preview if the first character of the match is
+        -- whitespace or is in the middle of an alphabetic word:
+        require('leap').opts.preview = function(ch0, ch1, ch2)
+            return not (
+                ch1:match('%s')
+                or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+            )
+        end
 
         -- indicate when leap is active
         vim.api.nvim_create_autocmd("User", {
